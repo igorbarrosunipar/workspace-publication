ddda685dff0ba8bd14f066fc7a04405347d2d9a1
igorbarrosunipar <barrosigor74@gmail.com>
2021-04-27 19:21:06 -0300

    second commit

---
+++ appPublication/src/app/components/publication/publication.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-publication',
+  templateUrl: './publication.component.html',
+  styleUrls: ['./publication.component.scss'],
+})
+export class PublicationComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
